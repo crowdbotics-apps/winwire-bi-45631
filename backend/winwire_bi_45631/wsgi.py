c8c704f81260d1c5a93d205edffd4dd370b2abcc
Crowdbotics <team@crowdbotics.com>
2024-01-26 22:50:00 +0000

    Initial commit

---
+++ backend/winwire_bi_45631/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for winwire_bi_45631 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "winwire_bi_45631.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winwire-bi-45631",
+  url: "https://winwire-bi-45631.botics.co",
+  api: "https://winwire-bi-45631.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
